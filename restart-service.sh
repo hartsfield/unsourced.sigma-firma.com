e5eb19fd5c7ab1eb5442303b48a93231eb95d1dd
hartsfield <johnathanhartsfield@gmail.com>
2023-06-16 08:50:47 -0400

    updates

--- restart-service.sh
+++ restart-service.sh
@@ -3,7 +3,7 @@ export servicePort=9395
 export logFilePath=./log_file.txt
 trap -- '' SIGTERM
 git pull
-go build -o boiler_plate
-pkill -f boiler_plate
-nohup ./boiler_plate > /dev/null & disown
+go build -o unsourced
+pkill -f unsourced
+nohup ./unsourced > /dev/null & disown
 sleep 2
